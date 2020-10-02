b3a0ac9067dbb909835814366ea2074513c37ace
Nihal Williams <nihalwill@gmail.com>
2020-10-02 11:42:50 -0400

    Save button, local storage features, display saved items all working

--- Develop/script.js
+++ Develop/script.js
@@ -8,3 +8,47 @@ $(document).ready(function () {
   }
   setInterval(updateTime, 1000);
 });
+
+$(".saveBtn").on("click", function () {
+  event.preventDefault();
+  console.log("test of save button");
+
+  var dayPlan = $(this).siblings(".description").val();
+  console.log("test: ", dayPlan);
+  var currentHour = $(this).siblings(".hour").text();
+  console.log("current hour: ", currentHour);
+  localStorage.setItem(currentHour, dayPlan);
+});
+
+var t8 = localStorage.getItem("8AM");
+$("#eight-am").text(t8);
+
+var t9 = localStorage.getItem("9AM");
+$("#nine-am").text(t9);
+
+var t10 = localStorage.getItem("10AM");
+$("#ten-am").text(t10);
+
+var t11 = localStorage.getItem("11AM");
+$("#eleven-am").text(t11);
+
+var t12 = localStorage.getItem("12PM");
+$("#tweleve-pm").text(t12);
+
+var t13 = localStorage.getItem("1PM");
+$("#one-pm").text(t13);
+
+var t14 = localStorage.getItem("2PM");
+$("#two-pm").text(t14);
+
+var t15 = localStorage.getItem("3PM");
+$("#three-pm").text(t15);
+
+var t16 = localStorage.getItem("4PM");
+$("#four-pm").text(t16);
+
+var t17 = localStorage.getItem("5PM");
+$("#five-pm").text(t17);
+
+var t18 = localStorage.getItem("6PM");
+$("#six-pm").text(t18);
--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Started the layout of the planner
 Added the Moment JS real-time feature to the jumbotron
 Began editing CSS and Bootstrap features for aesthetics
+
+---
+
+Save button feature working
+local storage of user input and recall working via set and get item values
